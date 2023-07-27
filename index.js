41c722e18c45b33afe0d2e86f19044d9d17c9fe4
Shaheer Malik <139039230+Malikshaheer222@users.noreply.github.com>
2023-07-27 08:37:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+let n = 5;
+console.log(n >= 5 && n < 10); //
+console.log(n > 5 && n < 10); //
+console.log(n >= 5 || n < 10); //
+console.log(n > 5 || n < 10); //
+console.log(!(n < 10)); //
+console.log(!(n > 10)); //
